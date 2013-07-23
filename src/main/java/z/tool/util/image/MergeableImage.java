b6771223a484f179c03906fee8e67ee6ec6c00cf
auzll <auzll@msn.com>
2013-07-23 21:41:11 +0800

    调整image工具类

--- src/main/java/z/tool/util/image/MergeableImage.java
+++ src/main/java/z/tool/util/image/MergeableImage.java
@@ -6,6 +6,7 @@ package z.tool.util.image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
@@ -14,21 +15,31 @@ import javax.imageio.ImageIO;
  * @since 2013-7-20 上午11:41:06
  */
 public final class MergeableImage implements Mergeable {
-    private File file;
-    private int x;
-    private int y;
+    private final InputStream inputStream;
+    private final File file;
+    private final int x;
+    private final int y;
 
-    private MergeableImage(File file, int x, int y) {
+    private MergeableImage(InputStream inputStream, File file, int x, int y) {
+        this.inputStream = inputStream;
         this.file = file;
         this.x = x;
         this.y = y;
     }
 
     public void draw(BufferedImage buffered) throws IOException {
-        buffered.getGraphics().drawImage(ImageIO.read(file), x, y, null);
+        if (null != inputStream) {
+            buffered.getGraphics().drawImage(ImageIO.read(inputStream), x, y, null);
+        } else if (null != file) {
+            buffered.getGraphics().drawImage(ImageIO.read(file), x, y, null);
+        }
     }
-
+    
+    public static MergeableImage of(InputStream inputStream, int x, int y) {
+        return new MergeableImage(inputStream, null, x, y);
+    }
+    
     public static MergeableImage of(File file, int x, int y) {
-        return new MergeableImage(file, x, y);
+        return new MergeableImage(null, file, x, y);
     }
 }
